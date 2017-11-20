e3dbe018bdd8faf14a38434ec34c0748cd33ed88
sasi <sasipa.wk@gmail.com>
2017-11-20 16:41:37 +0700

    add SQL statement

---
+++ Document/SQL/geo.sql
@@ -0,0 +1,18 @@
+﻿
+
+CREATE TABLE geo
+(
+  geo_id integer NOT NULL,
+  geo_name text,
+  CONSTRAINT geo_pkey PRIMARY KEY (geo_id)
+)
+
+
+INSERT INTO geo(
+            geo_id, geo_name)
+    VALUES (1, 'ภาคเหนือ'),
+(2, 'ภาคกลาง'),
+(3, 'ภาคตะวันออกเฉียงเหนือ'),
+(4, 'ภาคตะวันตก'),
+(5, 'ภาคตะวันออก'),
+(6, 'ภาคใต้');
